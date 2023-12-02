2c2f672ebea15990b931787f889524e86d762553
Leandro Marques <leandromarques.n@gmail.com>
2023-12-01 22:13:46 -0300

    feat: Adicionar recursos para login de usuario
    Changes to be committed:
    	new file:   Documents info/.info2.txt.kate-swp
    	modified:   Documents info/info2.txt
    	modified:   Routes/usuarios.js
    	modified:   nodemon.json
    	modified:   package-lock.json
    	modified:   package.json

--- Routes/usuarios.js
+++ Routes/usuarios.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 
 // ------- CRIANDO A ROTA DE CADASTRO USANDO O POST -----
 router.post('/cadastro', (req, resp, next) => {
@@ -50,8 +50,45 @@ router.post('/cadastro', (req, resp, next) => {
     });
 });
 
+// ------- CRIANDO A ROTA DE LOGIN USANDO O POST -----
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error1, conn) => {
 
+        if (error1) { return res.status(500).send({ error: error1, menss: "erro1" }) };
 
+        const query = 'select * from usuarios where email =?';
+
+        conn.query(query, [req.body.email], (error2, results, fields) => {
+            conn.release();
+            if (error2) { return res.status(500).send({ error: error2, menss: "erro2" }) };
+
+            if (results.length < 1) {
+                return res.status(401).send({ menssagem: "Falha na autenticação! error results.length < 0" });
+            }
+
+            // validando senhas string com a senha hash que é criptografada
+            bcrypt.compare(req.body.senha, results[0].senha, (error3, results) => {
+                if (error3) {
+                    return res.status(401).send({ menss: 'falha na autenticação!', menss: error3 });
+                }
+                if (results) {
+                    const token = jwt.sign({
+                        id_usuario: results.id_usuario,
+                        email: results.email
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+
+                        });
+                    return res.status(200).send({ menss: 'Autenticado com Sucesso!!', request: { email: req.body.email, menss: "token de acesso", token: token } });
+
+                }
+                return res.status(401).send({ menss: 'Falha na autenticação ' });
+            });
+        });
+    });
+})
 
 
 module.exports = router;
